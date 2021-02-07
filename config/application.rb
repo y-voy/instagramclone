c51f046e6d90e62c6b004c7a4a0f02fee2ca26df
yuji moriya <voyager.nasa0928@gmail.com>
2021-02-07 13:42:45 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
+module Instagramclone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+  end
+end
