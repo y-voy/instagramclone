cc6c5870fcfa79e669510cfd2647facabfb4524e
yuji moriya <voyager.nasa0928@gmail.com>
2021-02-11 18:33:27 +0900

    セッション機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :image, :image_cache, :password, :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :pictures do
     collection do
       post :confirm
     end
   end
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
